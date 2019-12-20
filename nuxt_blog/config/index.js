9e0136eac815e68779bb66982467e88a4f4a4f97
shmaur <shmaur@outlook.com>
2019-12-21 02:13:42 +0800

    index

--- nuxt_blog/config/index.js
+++ nuxt_blog/config/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: huangjin
  * @Date: 2019-12-17 23:49:08
- * @LastEditTime : 2019-12-20 21:53:50
+ * @LastEditTime : 2019-12-21 02:10:40
  * @LastEditors  : huangjin
  * @Description: 因为喜欢而坚持
  * @生活不止眼前的苟且，还有诗和远方
@@ -12,10 +12,10 @@ let baseURL = '';
 
 if (process.env.NODE_ENV == 'development') {
   baseURL = 'http://localhost:3005';
-  // baseImgPath = 'https://www.lzzj.online:5001/upload/';
+  // baseImgPath = 'http://localhost:3000/upload/';
 } else {
   baseURL = 'http://localhost:3000';
-  // baseImgPath = 'https://www.lzzj.online:5001/upload/';
+  // baseImgPath = 'http://localhost:3000/upload/';
 }
 
 export default baseURL
